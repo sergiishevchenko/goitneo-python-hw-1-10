156c74f118e879c8219a6711717a4579fd4a1b8c
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-09-22 15:06:16 +0200

    fixed according Vladimir comments | 1

--- main.py
+++ main.py
@@ -25,21 +25,12 @@ def get_birthdays_per_week(users):
         name = user['name']
         birthday = user['birthday']
         birthday_this_year = birthday.replace(year=today.year)
-        if birthday_this_year >= today:
-            delta_days = (birthday_this_year - today).days
-            if delta_days < 7:
-                if birthday.weekday() == 0:
-                    result['Monday'].append(name)
-                elif birthday.weekday() == 1:
-                    result['Tuesday'].append(name)
-                elif birthday.weekday() == 2:
-                    result['Wednesday'].append(name)
-                elif birthday.weekday() == 3:
-                    result['Monday'].append(name)
-                elif birthday.weekday() == 4:
-                    result['Thursday'].append(name)
-                else:
-                    result['Friday'].append(name)
+        delta_days = (birthday_this_year - today).days
+        if birthday_this_year >= today and delta_days < 7:
+            if birthday.strftime('%A') in result.keys():
+                result[birthday.strftime('%A')].append(name)
+            else:
+                result['Monday'].append(name)
 
     for key, value in result.items():
         if value:
